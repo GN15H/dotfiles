b0d7a0818125856bd7fb62050c91063fde48cd02
Cristobal Ochoa <cristobalochoacadavid@gmail.com>
2026-05-14 10:07:25 -0500

    modularization of hyprland lua files

---
+++ hypr/modules/monitors.lua
@@ -0,0 +1,21 @@
+hl.monitor({
+	output = "",
+	mode = "preferred",
+	position = "auto",
+	scale = "auto",
+})
+
+--Two monitors (uncomment for usage)
+-- hl.monitor({
+-- 	output = "DP-1",
+-- 	mode = "1920x1080",
+-- 	position = "0x0",
+-- 	scale = "1",
+-- })
+--
+-- hl.monitor({
+-- 	output = "DP-2",
+-- 	mode = "1920x1080",
+-- 	position = "1920x0",
+-- 	scale = "auto",
+-- })
---
+++ hypr/modules/variables.lua
@@ -0,0 +1,7 @@
+return {
+	terminal = "kitty",
+	fileManager = "dolphin",
+	menu = "wofi",
+	power_menu = "~/.scripts/powermenu.sh",
+	background = "~/Documents/h2igPl.jpg",
+}
