818baed76a97909f38df09e90de5a3a7ecf863d3
Cristobal Ochoa <cristobalochoacadavid@gmail.com>
2026-06-22 11:21:20 -0500

    bruh

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,10 +2,11 @@ require 'config.options'
 require 'config.keybinds'
 require 'config.lazy'
 
-vim.lsp.enable({
-    'clangd',
-    'typescript',
-    'lua_ls',
-    'pyright',
-    'cssls'
-})
+vim.lsp.enable {
+  'clangd',
+  'typescript',
+  'lua_ls',
+  'pyright',
+  'cssls',
+  'terraformls',
+}
